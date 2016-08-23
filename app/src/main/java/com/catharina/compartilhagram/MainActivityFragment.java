2db675beacef8aaa62a1798610fc3c2069390f20
Catharina <catha.ana.1994@gmail.com>
2016-08-23 15:07:25 -0300

    Função deletar

--- app/src/main/java/com/catharina/compartilhagram/MainActivityFragment.java
+++ app/src/main/java/com/catharina/compartilhagram/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,8 +15,10 @@ import android.view.ViewGroup;
 
 import com.catharina.compartilhagram.dominio.adapter.PostAdapter;
 import com.catharina.compartilhagram.dominio.dao.DAO;
+import com.catharina.compartilhagram.dominio.modelo.Post;
 
 import io.realm.Realm;
+import io.realm.RealmResults;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -27,6 +30,7 @@ public class MainActivityFragment extends Fragment {
     private PostAdapter adapter;
     private Realm realm;
     private DAO dao;
+    private RealmResults<Post> posts;
 
     public MainActivityFragment() {
     }
@@ -39,12 +43,30 @@ public class MainActivityFragment extends Fragment {
         realm = Realm.getDefaultInstance();
         dao = DAO.getInstance(realm);
 
-        adapter = new PostAdapter(getContext(), dao.getPostAll());
+        posts = dao.getPostAll();
+        adapter = new PostAdapter(getContext(), posts);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.listaPosts);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(adapter);
+
+        ItemTouchHelper swipeToDismissTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                realm.beginTransaction();
+                posts.get(viewHolder.getAdapterPosition()).deleteFromRealm();
+                realm.commitTransaction();
+                adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+        });
+        swipeToDismissTouchHelper.attachToRecyclerView(recyclerView);
 
         return view;
     }
